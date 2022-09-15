19f8fde527c06d29ccf78f024c08b39f5aef3e08
Gabriel Martins Rennó <gabrielmrenno@gmail.com>
2022-09-14 21:24:40 -0300

    Second Class Finished

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,7 +1,27 @@
-import { StatusBar } from 'react-native';
+import { StatusBar, Text } from 'react-native';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black
+} from '@expo-google-fonts/inter'
+
+import { Home } from './src/screens/Home'
+
 import { Background } from './src/components/Background'
+import { Loading } from './src/components/Loading'
 
 export default function App() {
+
+  // fontsLoaded is true if the font is loaded
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black
+  });
+
   return (
     <Background>
       {/* To set Status bar as transparent with background of our App*/}
@@ -10,6 +30,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
+      {fontsLoaded ? <Home /> : <Loading />}
     </Background>
   );
 }
